450d402c7b8fc54d988cc84addfd6f219e63a0d9
henryzhuhr <henry.zhuhr@icloud.com>
2026-02-15 11:29:52 +0800

    ✨feat: 更新 Logger 协议，增加 warning 和 error 方法的重载定义

--- src/log/interface.py
+++ src/log/interface.py
@@ -3,19 +3,25 @@ from typing import Any, Protocol, overload
 
 class Logger(Protocol):
     @overload
-    def debug(__self, __message: str, *args: Any, **kwargs: Any) -> None: ...  # noqa: N805
+    def debug(__self, __message: str, *args: Any, **kwargs: Any) -> None: ...
 
     @overload
-    def debug(__self, __message: Any) -> None: ...  # noqa: N805
+    def debug(__self, __message: Any) -> None: ...
 
     @overload
-    def info(__self, __message: str, *args: Any, **kwargs: Any) -> None: ...  # noqa: N805
+    def info(__self, __message: str, *args: Any, **kwargs: Any) -> None: ...
 
     @overload
-    def info(__self, __message: Any) -> None: ...  # noqa: N805
+    def info(__self, __message: Any) -> None: ...
 
     @overload
-    def error(__self, __message: str, *args: Any, **kwargs: Any) -> None: ...  # noqa: N805
+    def warning(__self, __message: str, *args: Any, **kwargs: Any) -> None: ...
 
     @overload
-    def error(__self, __message: Any) -> None: ...  # noqa: N805
+    def warning(__self, __message: Any) -> None: ...
+
+    @overload
+    def error(__self, __message: str, *args: Any, **kwargs: Any) -> None: ...
+
+    @overload
+    def error(__self, __message: Any) -> None: ...
